df874918a565253aa92281614ad4c7dfad40a6d9
Antho <anthony@wizbii.com>
2014-07-29 11:27:23 +0200

    Modif composer

--- src/Acme/CalculatorBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Acme/CalculatorBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,8 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/compute?left=3&operator=add&right=2');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("= 5")')->count() > 0);
     }
 }
